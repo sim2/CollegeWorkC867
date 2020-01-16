3fa4ce4de903a581b4f4c67cdc2d8952d5fe4954
Simcha Hochman <simchahochman@yahoo.com>
2020-01-15 19:54:57 -0500

    V 1.0

---
+++ Scripting_and_Programming_Applications_C867/Student.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Student {
+public:
+	void FirstName;
+	void LastName :
+	void emailAddress;
+	void age;
+	void daysToComplete :
+	void degreeType  //Note: Degree type should be populated in subclasses only.
+
+};
