19cfa2ee7fb9622c5ac632d608b5e17719afd478
Simcha Hochman <simchahochman@yahoo.com>
2020-01-26 21:44:17 -0500

    2.e

--- Scripting_and_Programming_Applications_C867/Student.cpp
+++ Scripting_and_Programming_Applications_C867/Student.cpp
@@ -28,6 +28,7 @@ public:
 	array  GetDaysToComplete() const;
 	string  GetDegreeType() const;
 
+	return;
 };
 
 	// 2.c
@@ -38,47 +39,28 @@ Student::Student() {
 	FirstName = "";
 	LastName = "";
 	EmailAddress = "";
-	Age = "";
-	DaysToComplete = "";
+	Age = "-1";
+	DaysToComplete = {};
 	DegreeType = "";
+};
+
+// 2.e
+
+Student::~Student() {
+
+	delete StudentID;
+	delete FirstName;
+	delete LastName;
+	delete EmailAddress;
+	delete Age;
+	delete DaysToComplete;
+	delete DegreeType;
 };
 
 	//2.d
 
-cout << Student << endl;
+cout << studentData << endl;
 
-	// 2.e
-
-class MyClass {
-public:
-	MyClass();
-	~MyClass();
-private:
-	int* subObj;
-};
-
-MyClass::MyClass() {
-	cout << "Constructor called." << endl;
-	subObj = new int; // Allocate mem for data
-	*subObj = 0;
-	return;
-}
-
-MyClass::~MyClass() {
-	cout << "Destructor called." << endl;
-	delete subObj;
-	return;
-}
-
-int main() {
-	MyClass* tempClassObj;      // Create object of type MyClass
-
-	tempClassObj = new MyClass; // Allocate mem for object
-	delete tempClassObj;        // No more memory leak
-								// Freed obj's mem, including subObj
-	// Rest of program ...
-	return 0;
-}
 
 	//2.f what do i do? -  virtual getDegreeProgram()
 Student.GetDegreeProgram(); 
