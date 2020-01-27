baf5e7808b2a62580f2584fa3b89918b3f3a6ec6
Simcha Hochman <simchahochman@yahoo.com>
2020-01-26 20:38:17 -0500

    Added curly brackets to data array

--- Scripting_and_Programming_Applications_C867/Student.cpp
+++ Scripting_and_Programming_Applications_C867/Student.cpp
@@ -91,10 +91,10 @@ Student.GetDegreeProgram();
 -Student Data-
 
 const string studentData[] =
-	"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-	"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-	"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-	"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-	"A5,Simcha,Hochman,shochm1@wgu.edu,27,40,50,21,SOFTWARE"
-
+	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+	  "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+	  "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+	  "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+	  "A5,Simcha,Hochman,shochm1@wgu.edu,27,40,50,21,SOFTWARE"
+	};
 */
