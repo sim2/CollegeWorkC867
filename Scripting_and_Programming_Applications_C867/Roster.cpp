f916d7a47951162e94e10db73457830c132f05ec
Simcha Hochman <simchahochman@yahoo.com>
2020-01-27 23:11:26 -0500

    Middle of E

--- Scripting_and_Programming_Applications_C867/Roster.cpp
+++ Scripting_and_Programming_Applications_C867/Roster.cpp
@@ -4,14 +4,13 @@
 class Roster{
 
 public:
-	array classRosterArray{};
+	Student classRosterArray[5];
 	
 };
 
 
 // F
 /*
-1.  Print out to the screen, via your application, the course title, the programming language used, your student ID, and your name.
 
 2.  Create an instance of the Roster class called classRoster.
 
@@ -32,6 +31,20 @@ classRoster.remove("A3");
 */
 
 void main() {
+
+	const string studentData[] =
+	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+	  "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+	  "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+	  "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+	  "A5,Simcha,Hochman,shochm1@wgu.edu,27,40,50,21,SOFTWARE"
+	};
+
+	// F.1
+	cout << "Scripting and Programming Applications using C++" << endl; 
+	cout << "Student ID = 858824 Name = Simcha Hochman" endl;
+
+	
 	Student Student1;
 
 	Student1.Print();
@@ -45,3 +58,7 @@ void main() {
 }
 
 
+//for each string in studentData{
+//	take each string and break it up into individual strings
+//};
+// classRoster.printAll will cout (print to console) each object, each student class (networkStudent, softwareStudent, securityStudent)
