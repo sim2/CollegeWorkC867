d9b149dd62c9621cf09e239c119ab2fbef58d38f
Simcha Hochman <simchahochman@yahoo.com>
2020-01-31 16:11:54 -0500

    Last Changes

--- Scripting_and_Programming_Applications_C867/Roster.cpp
+++ Scripting_and_Programming_Applications_C867/Roster.cpp
@@ -18,6 +18,110 @@ public:
 	
 };
 
+//Updating RosterArray
+void Roster::add(string studentID, string FirstName, string LastName, string EmailAddress, int Age, int DaysToComplete1, int DaysToComplete2, int DaysToComplete3, Degree DegreeType) {
+	int DaysToComplete[] = { DaysToComplete1, DaysToComplete2, DaysToComplete3 };
+	if (degree == Degree::NETWORKING) {
+		classRosterArray[addIndex++] = new NetworkStudent(studentID, FirstName, LastName, EmailAddress, Age, DaysToComplete, DegreeType);
+	}
+	if (degree == Degree::SECURITY) {
+		classRosterArray[addIndex++] = new SecurityStudent(studentID, FirstName, LastName, EmailAddress, Age, DaysToComplete, DegreeType);
+	}
+	if (degree == Degree::SOFTWARE) {
+		classRosterArray[addIndex++] = new SoftwareStudent(studentID, FirstName, LastName, EmailAddress, Age, DaysToComplete, DegreeType);
+	}
+}
+
+//printing entire roster
+void Roster::printAll() {
+	cout << "Displaying roster:" << '\n';
+	cout << endl;
+	for (int i = 0; i < 5; i++) {
+		(*classRosterArray[i]).print();
+	}
+	cout << '\n';
+}
+
+//printing the average days it took to complete 3 courses
+void Roster::printnumDays(string studentID) {
+	for (int i = 0; i < 5; i++) {
+		if ((*classRosterArray[i]).getStudentID() == studentID) {
+			int avg = 0;
+			avg = ((*classRosterArray[i]).getNumDays()[0] + (*classRosterArray[i]).getNumDays()[1]
+				+ (*classRosterArray[i]).getNumDays()[2]) / 3;
+			cout << "The average days it took the student with studentID: " << studentID << " to finish 3 courses: " << avg << '\n';
+		}
+	}
+	cout << '\n';
+}
+
+// Printing studnts who are in a specific degree program
+void Roster::printByDegree(string degree) {
+	Degree testDeg = Degree::SOFTWARE;
+	if (degree == "SECURITY") {
+		testDeg = Degree::SECURITY;
+		cout << "Displaying students in the Security degree: " << endl;
+	}
+	if (degree == "NETWORKING") {
+		testDeg = Degree::NETWORKING;
+		cout << "Displaying students in the Networking degree: " << endl;
+	}
+	if (degree == "SOFTWARE") {
+		testDeg = Degree::SOFTWARE;
+		cout << "Displaying students in the Software degree: " << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 5; i++) {
+		if (testDeg == (*classRosterArray[i]).getDegree()) {
+			(*classRosterArray[i]).print();
+		}
+	}
+	cout << '\n';
+}
+
+// Checking Emails and displayign invalid ones
+void Roster::printBadEmails() {
+	cout << "Displaying invalid emails:" << '\n';
+	cout << endl;
+	for (int i = 0; i < 5; i++) {
+		bool foundCharAt = false;
+		bool foundCharPeriod = false;
+		bool foundCharSpace = false;
+		string email = "";
+		email = (*classRosterArray[i]).getEmailAddress();
+		for (char &c : email) {
+			if (c == '@') {
+				foundCharAt = true;
+			}
+			if (c == '.') {
+				foundCharPeriod = true;
+			}
+			if (c == ' ') {
+				foundCharSpace = true;
+			}
+		}
+		if (foundCharAt == false || foundCharPeriod == false || foundCharSpace == true) {
+			cout << (*classRosterArray[i]).getEmailAddress() << '\n';
+		}
+	}
+	cout << '\n';
+}
+
+// Removing student from the roster by Student ID
+void Roster::remove(string studentID) {
+	bool removed = false;
+	for (int i = 0; i < 5; i++) {
+		if (classRosterArray[i] != NULL) {
+			if (studentID == classRosterArray[i]->getStudentID()) {
+				classRosterArray[i] = nullptr;
+				removed = true;
+			}
+		}
+	}
+	if (!removed) {
+		cout << "The student with the ID: " << studentID << " was not found." << '\n';
+	}
+}
 
 // F
 /*
@@ -57,17 +161,55 @@ void main() {
 
 	// iterate through student data
 	std::string studentData = "";
-	std::istringstream ss(studentData);
+	std::istringstream eachStudent(studentData);
 	std::string token;
 
-	while (std::getline(ss, token, ',')) {
-		std::cout << token << '\n';
-	}
 
 	// Copy studentData to New strings
 	for (i = 0; i < 5; i++) {
-		stringstream ss (studentData[i])
+		stringstream eachStudent(studentData[i])
+
+			array <string > endString;
+
+			while (eachStudent.good()) {
+				string indivString;
+					getline(eachStudent, endString, ",");
+					result.push_back(indivString);
+			}
+
 	}
+
+	if (endString[8] == "SECURITY") {
+		myDegree = Degree::SECURITY;
+	}
+	if (endString[8] == "SOFTWARE") {
+		myDegree = Degree::SOFTWARE;
+	}
+	if (endString[8] == "NETWORK") {
+		myDegree = Degree::NETWORKING;
+	}
+	classRoster.add(endString[0], endString[1], endString[2], endString[3], stoi(endString[4]), stoi(endString[5]),
+		stoi(endString[6]), stoi(endString[7]), myDegree);
+}
+
+
+// print everything
+classRoster.printAll();
+
+classRoster.printBadEmails();
+
+classRoster.printnumDays("A1");
+
+classRoster.printByDegree("SOFTWARE");
+
+classRoster.remove("A3");
+
+classRoster.remove("A3");
+}
+
+//Deconstructor
+Roster::~Roster() {}
+
 
 
 	/*
@@ -79,12 +221,6 @@ void main() {
 	Student1.Print();
 
 	delete Student;
-						*/
+						
 	return 0;      
 }
-
-
-//for each string in studentData{
-//	take each string and break it up into individual strings
-//};
-// classRoster.printAll will cout (print to console) each object, each student class (networkStudent, softwareStudent, securityStudent)
