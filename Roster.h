a07a553492a131e92a8ae6397a2a867da0535e5e
Simcha Hochman <simchahochman@yahoo.com>
2020-02-03 22:47:34 -0500

    pushed

---
+++ Roster.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Student.h"
+#include <arrray>
+
+class Roster {
+
+public:
+	Roster();
+
+	virtual ~Roster();
+
+	void printAll();
+
+	void add(string studentID, string FirstName, string LastName, string EmailAddress, int Age, int DaysToComplete1, int DaysToComplete2, int DaysToComplete3, DegreeType);
+
+	void remove(string studentID);
+
+	void printByDegree(string DegreeType);
+
+	void printDaysToComplete(string);
+
+	void printBadEmails();
+
+	int addIndex = 0;
+
+private:
+
+	Student *classRosterArray[5];
+};
+};
